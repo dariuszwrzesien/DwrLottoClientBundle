9f98699b3f0de1dc65de54d0935848ce55c29006
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2015-07-31 00:32:48 +0200

    added test

---
+++ Tests/Service/LottoClientTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dwr\LottoClientBundle\Tests;
+
+
+class LottoClientTest extends \PHPUnit_Framework_TestCase
+{
+    public function testNumberOfResultsFromGetRecentlyResults()
+    {
+        $this->assertTrue(true);
+    }
+}
